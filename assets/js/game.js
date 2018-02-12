f6efe274f5cc176523c4abac9433c6e12febd566
SiriusScaper <siriusscaper@gmail.com>
2018-02-12 13:11:12 -0800

    Added more logic outline

---
+++ assets/js/game.js
@@ -0,0 +1,66 @@
+variables
+// 
+
+//Object for char properties/values
+function Char(attk, counterAttk, health, charImg){
+    this.attk = attk
+    this.counterAttk = counterAttk
+    this.health = health
+    this.charImg = charImg
+}
+
+//Characters
+let obiwan = new Char(6, 15, 120, url);
+let vader = new Char(10, 25, 150, url);
+let luke = new Char(8, 12, 100, url);
+let themperor = new Char(9, 8, 160, url);
+
+//Win state is defeating remaining characters
+
+
+function gameSetup(){
+    //setup start state
+    //characters represented with images in the DOM
+    //characters are selectable
+    //Score is 0?
+}
+
+function addImages(){
+    //add images to html
+}
+
+function selectChar(){
+
+    //player selects a character to fight as and against
+    //apply a colored border to indicate selection
+    //First selection is PC
+    //Second selection is enemy
+}
+
+function playerChar(){
+    //apply stats and use pcAttackPower
+}
+
+//PC = Player Character
+function pcAttackPower(){
+    //handle attack function for character recursively attack+=
+    
+}
+
+function enemyChar(){
+    //apply stats
+}
+
+
+function attack(){
+
+    //handle attacking & counter attacking
+    //attack is property health - attk
+    //counter attack is the health - counterAttk property
+}
+
+function addtoHTML(){
+
+}
+
+
