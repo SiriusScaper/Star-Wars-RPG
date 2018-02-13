1d03d81232578d924ec001aae40b97f38a5e5d63
SiriusScaper <siriusscaper@gmail.com>
2018-02-12 22:41:59 -0800

    Failing at actually coding

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,68 +3,71 @@
 
 //Object for char properties/values
 function Char(attk, counterAttk, health, charImg){
-    this.attk = attk
-    this.counterAttk = counterAttk
-    this.health = health
-    this.charImg = charImg
+	this.attk = attk
+	this.counterAttk = counterAttk
+	this.health = health
+	this.charImg = new Image(200, 200)
 }
 
 //Characters
-let obiwan = new Char(6, 15, 120, url)
-let vader = new Char(10, 25, 150, url)
-let luke = new Char(8, 12, 100, url)
-let themperor = new Char(9, 8, 160, url)
+let obiwan = new Char(6, 15, 120, '')
+let vader = new Char(10, 25, 150, '')
+let luke = new Char(8, 12, 100, '')
+let themperor = new Char(9, 8, 160,'')
 
 //Win state is defeating remaining characters
 
 
 function gameSetup(){
-    //setup start state
-    //characters represented with images in the DOM
-    //characters are selectable
-    //Score is 0?
+	//setup start state
+	//characters represented with images in the DOM
+	//characters are selectable
+	//Score is 0?
 }
 
 
 function addtoHTML(){
-    //This will add elements to html for other functions 
+	//This will add elements to html for other functions
 }
 
+
 function remove(){
-    //this will remove elements from the html for other functions
+	//this will remove elements from the html for other functions
 }
 
 function addImages(){
-    //add images to html
+	//add images to html
 }
 
 function selectChars(){
-    
-    //player selects a character to fight as and against
-    //apply a colored border to indicate selection
-    //First selection is PC
-    //Second selection is enemy
+	
+	//player selects a character to fight as and against
+	$(document).on('click', function select() {
+	})
+	//apply a colored border to indicate selection
+	//First selection is PC
+	//Second selection is enemy
 }
 
 function playerChar(){
-    //apply stats and use pcAttackPower
+	//apply stats and use pcAttackPower
 }
 
 function enemyChar(){
-    //apply stats
+	//apply stats
 }
 
 function attack(){
 
-    //handle attacking & counter attacking
-    //attack is property health - attk
-    //counter attack is the health - counterAttk property
+	//handle attacking & counter attacking
+	//attack is property health - attk
+	//counter attack is the health - counterAttk property
 }
 
 //PC = Player Character
 function pcAttackPower(){
-    //handle attack function for character recursively attack+=
-    
+	//handle attack function for PC = attack+=
+	
 }
 
 
